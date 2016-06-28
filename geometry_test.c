faeb549a5bbd3d009482f5ba475dd82ca9ec6d7e
kybu3653 <kybu3653@colorado.edu>
2016-06-28 12:13:24 -0600

    add function

--- geometry_test.c
+++ geometry_test.c
@@ -12,7 +12,7 @@
 
 #include <stdlib.h>
 #include <check.h>
-
+#include <stdio.h>
 #include "geometry.h"
 
 /* coord_2d_eq Test */
@@ -145,6 +145,22 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_tri_area)
+{
+   //define vars
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+   
+   a.x = b.x = a.y = 0;
+   b.y = c.y = 5;
+   c.x = 4;
+
+   double num = coord_2d_area_triangle(&a,&b,&c);
+   ck_assert(num==(10.0));
+   
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,11 +178,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+   TCase* tc_area_triangle = tcase_create("coord_2d_area_triangle");
+   tcase_add_test(tc_area_triangle, test_tri_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+   suite_add_tcase(s, tc_area_triangle);
     /* Return Suite */
     return s;
 
